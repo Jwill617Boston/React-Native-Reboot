45fc97ec6ab1d5d2a0bf84cc385fb9a0ff8ab52b
Jason Williams <jwilliams617bos@gmail.com>
2021-07-30 11:26:01 -0400

    Wk1

---
+++ components/ContactComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+
+ class Contact extends Component {
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+    render() {
+        return (
+            <ScrollView>
+                <Card title={"Community Partners"} wrapperStyle={{margin: 20}}>          
+                    <Text style={{marginBottom: 10}}>
+                    1 Nucamp Way
+                    Seattle, WA 98001
+                    U.S.A.
+                    Phone: 1-206-555-1234
+                    Email: campsites@nucamp.co
+                    </Text>
+                </Card>          
+            </ScrollView>
+        )
+    }
+}
+
+export default Contact;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import Constants from 'expo-constants';
-import { View, Platform, SafeAreaView } from 'react-native';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
+import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
-        Directory: { screen: Directory},
-        CampsiteInfo: { screen: CampsiteInfo}
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
     },
     {
-        initalRouteName: 'Directory',
+        initialRouteName: 'Directory',
         defaultNavigationOptions: {
             headerStyle: {
                 backgroundColor: '#5637DD'
@@ -23,23 +26,82 @@ const DirectoryNavigator = createStackNavigator(
             }
         }
     }
-
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
 
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
 
-class Main extends Component { 
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
 
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator },
+        About: { screen: AboutNavigator },
+        Contact: { screen: ContactNavigator },
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
+const AppNavigator = createAppContainer(MainNavigator)
+
+class Main extends Component {
     render() {
         return (
-            <SafeAreaView
-                style={{
-                    flex: 1,
-                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight 
-                }}>
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+            }}>
                 <AppNavigator />
-            </SafeAreaView>
+            </View>
         );
     }
 }
