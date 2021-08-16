f457b3821d5735d20a0552299f21ec46c440db51
Jason Williams <jwilliams617bos@gmail.com>
2021-08-16 17:20:18 -0400

    camera login

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,109 +1,153 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList,
-    Modal, Button, StyleSheet,
-    Alert, PanResponder } from 'react-native';
-import { Card, Icon, Rating, Input } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
-import { postFavorite, postComment } from '../redux/ActionCreators';
-import * as Animatable from 'react-native-animatable';
+import {
+   Text,
+   View,
+   ScrollView,
+   FlatList,
+   Modal,
+   Button,
+   StyleSheet,
+   Alert,
+   PanResponder,
+   Share,
+} from "react-native";
+import { Card, Icon, Rating, Input } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import { postFavorite, postComment } from "../redux/ActionCreators";
+import * as Animatable from "react-native-animatable";
 
-const mapStateToProps = state => {
-    return {
-        campsites: state.campsites,
-        comments: state.comments,
-        favorites: state.favorites
-    };
+const mapStateToProps = (state) => {
+   return {
+      campsites: state.campsites,
+      comments: state.comments,
+      favorites: state.favorites,
+   };
 };
 
 const mapDispatchToProps = {
-    postFavorite: campsiteId => (postFavorite(campsiteId)),
-    postComment:  (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
+   postFavorite: (campsiteId) => postFavorite(campsiteId),
+   postComment: (campsiteId, rating, author, text) =>
+      postComment(campsiteId, rating, author, text),
 };
 
 function RenderCampsite(props) {
+   const { campsite } = props;
 
-    const {campsite} = props;
+   const view = React.createRef();
 
-    const view = React.createRef();
+   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
 
-    const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+   const recognizeComment = ({ dx }) => (dx > 200 ? true : false);
 
-    const recognizeComment = ({dx}) => (dx > 200) ? true : false;  
+   const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+         view.current
+            .rubberBand(1000)
+            .then((endState) =>
+               console.log(endState.finished ? "finished" : "canceled")
+            );
+      },
+      onPanResponderEnd: (e, gestureState) => {
+         console.log("pan responder end", gestureState);
+         if (recognizeDrag(gestureState)) {
+            Alert.alert(
+               "Add Favorite",
+               "Are you sure you wish to add " +
+                  campsite.name +
+                  " to favorites?",
+               [
+                  {
+                     text: "Cancel",
+                     style: "cancel",
+                     onPress: () => console.log("Cancel Pressed"),
+                  },
+                  {
+                     text: "OK",
+                     onPress: () =>
+                        props.favorite
+                           ? console.log("Already set as a favorite")
+                           : props.markFavorite(),
+                  },
+               ],
+               { cancelable: false }
+            );
+         } else if (recognizeComment(gestureState)) {
+            props.onShowModal();
+         }
+         return true;
+      },
+   });
 
-    const panResponder = PanResponder.create({
-        onStartShouldSetPanResponder: () => true,
-        onPanResponderGrant: () => {
-            view.current.rubberBand(1000)
-            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
-        },
-        onPanResponderEnd: (e, gestureState) => {
-            console.log('pan responder end', gestureState);
-            if (recognizeDrag(gestureState)) {
-                Alert.alert(
-                    'Add Favorite',
-                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
-                    [
-                        {
-                            text: 'Cancel',
-                            style: 'cancel',
-                            onPress: () => console.log('Cancel Pressed')
-                        },
-                        {
-                            text: 'OK',
-                            onPress: () => props.favorite ?
-                                console.log('Already set as a favorite') : props.markFavorite()
-                        }
-                    ],
-                    { cancelable: false }
-                );
-            }            
-            else if (recognizeComment(gestureState)){
-                props.onShowModal()
-            }
-            return true;
-        }
-    });
+   const shareCampsite = (title, message, url) => {
+      Share.share(
+         {
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url,
+         },
+         {
+            dialogTitle: "Share " + title,
+         }
+      );
+   };
 
-
-    if (campsite) {
-        return (
-            <Animatable.View
-                animation='fadeInDown'
-                duration={2000}
-                delay={1000}
-                ref={view}
-                {...panResponder.panHandlers}>
+   if (campsite) {
+      return (
+         <Animatable.View
+            animation="fadeInDown"
+            duration={2000}
+            delay={1000}
+            ref={view}
+            {...panResponder.panHandlers}
+         >
             <Card
-                featuredTitle={campsite.name}
-                image={{uri: baseUrl + campsite.image}}>
-                <Text style={{margin: 10}}>
-                    {campsite.description}
-                </Text>
-                <View style={styles.cardRow}>
-                    <Icon 
-                    name={props.favorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    reverse
-                    onPress={() => props.favorite ?
-                        console.log('Already set as favorite') : props.markFavorite()}
-                    />
-                    <Icon 
-                    name='pencil'
-                    type='font-awesome'
-                    color= '#5637DD' 
-                    raised
-                    reverse
-                    onPress={() => props.onShowModal()}
-                    />
-                </View>                
+               featuredTitle={campsite.name}
+               image={{ uri: baseUrl + campsite.image }}
+            >
+               <Text style={{ margin: 10 }}>{campsite.description}</Text>
+               <View style={styles.cardRow}>
+                  <Icon
+                     name={props.favorite ? "heart" : "heart-o"}
+                     type="font-awesome"
+                     color="#f50"
+                     raised
+                     reverse
+                     onPress={() =>
+                        props.favorite
+                           ? console.log("Already set as favorite")
+                           : props.markFavorite()
+                     }
+                  />
+                  <Icon
+                     name="pencil"
+                     type="font-awesome"
+                     color="#5637DD"
+                     raised
+                     reverse
+                     onPress={() => props.onShowModal()}
+                  />
+                  <Icon
+                     name={"share"}
+                     type="font-awesome"
+                     color="#5637DD"
+                     raised
+                     reverse
+                     onPress={() =>
+                        shareCampsite(
+                           campsite.name,
+                           campsite.description,
+                           baseUrl + campsite.image
+                        )
+                     }
+                  />
+               </View>
             </Card>
-            </Animatable.View>
-        );
-    }
-    return <View />;
+         </Animatable.View>
+      );
+   }
+   return <View />;
 }
 
 function RenderComments({comments}) {
